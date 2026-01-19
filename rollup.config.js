0eeac48f270bed79276c0fddd23be1d60fba9de8
ARIJIT BARIK <arijitbarik655@gmail.com>
2026-01-19 21:31:02 +0530

    new package update

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,32 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss"
+
 
 export default defineConfig({
   input: "src/index.ts",
   output: [
     {
       dir: "dist",
-      format: "es",
+      format: "esm",
       preserveModules: true,
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
-  external: [
-    "react",
-    "react-dom",
-    "tailwindcss",
-    "lucide-react"
-  ],
+  external: ["react", "react-dom", "tailwindcss", "lucide-react"],
   plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    postcss({
+      extract: true,        // ⬅️ creates dist/index.css
+      minimize: true,
+    }),
     typescript({
-      tsconfig: "./tsconfig.json"
-    })
-  ]
+      tsconfig: "./tsconfig.json",
+    }),
+  ],
 });
